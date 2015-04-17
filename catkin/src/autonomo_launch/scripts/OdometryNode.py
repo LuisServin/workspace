97e8b871941afd53dbfc9570c0b477aa4bfe8052
Luis Alfredo Servin Garduño <luis.alfredo.sega@gmail.com>
2015-04-17 16:04:19 -0500

    upgrade OdometryNode

--- catkin/src/autonomo_launch/scripts/OdometryNode.py
+++ catkin/src/autonomo_launch/scripts/OdometryNode.py
@@ -10,14 +10,16 @@ class Odometry:
 	def __init__(self, node_name = 'odometryNode'):
 		rospy.init_node(node_name)
 		self.node_name = rospy.get_name()
-		rospy.loginfo('started node %s', self.node_name)
 		rospy.Subscriber('encTicks', Vector3, self.callback)
+		self.rate = rospy.get_param("rate", 5)
+		rospy.loginfo("Rate for this node is " + str(self.rate))
+		rospy.loginfo("started node " + self.node_name)
 
 		# initialize all the variables in class
 
 		# robot properties
-		self.wheelRadius = 0 # [m]
-		self.wheelBase = 0 # [m]
+		self.wheelRadius = 0.1012 # [m]
+		self.wheelBase = 0.207 # [m]
 
 		self.velocityRigthWheel = 0
 		self.velocityLeftWheel = 0
@@ -42,18 +44,21 @@ class Odometry:
 		self.lastTime = rospy.get_time()
 		
 	def callback(self, data):
-		rospy.loginfo("Encoder ticks are " + str(data.x) + " and " + str(data.y))
-		calculateVelocities(data)
+		# rospy.loginfo("Encoder ticks are " + str(data.x) + " and " + str(data.y))
+		self.calculateVelocities(data)
 
 	def calculateVelocities(self, encoderTicks):
 		self.currentTime = rospy.get_time()
-		self.dt = currentTime - lastTime		
+		self.dt = self.currentTime - self.lastTime		
 
 		# calculate velocity 
-		self.velocityLeftWheel = data.x
+		self.velocityLeftWheel = encoderTicks.x
 
 	# ICC -> Instantaneous Center of Curvature
 	# def computeICC():
+
+	# def spin(self):
+
 
 if __name__ == '__main__':
 	odometry = Odometry()
