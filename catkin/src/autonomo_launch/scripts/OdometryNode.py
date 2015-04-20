854c1e16fffd2312404604cdd05f30b10146ef9b
Luis Alfredo Servin Garduño <luis.alfredo.sega@gmail.com>
2015-04-20 16:29:16 -0500

    Odometry node small upgrade

--- catkin/src/autonomo_launch/scripts/OdometryNode.py
+++ catkin/src/autonomo_launch/scripts/OdometryNode.py
@@ -3,6 +3,7 @@
 import rospy
 from geometry_msgs.msg import Vector3
 from nav_msgs.msg import Odometry
+from math import *
 
 class Odometry:
 	"""This class is inteded to calculate odometry message from a 
@@ -14,19 +15,28 @@ class Odometry:
 		self.rate = rospy.get_param("rate", 5)
 		rospy.loginfo("Rate for this node is " + str(self.rate))
 		rospy.loginfo("started node " + self.node_name)
+		rospy.loginfo("Pi value is " + str(pi))
 
 		# initialize all the variables in class
 
 		# robot properties
 		self.wheelRadius = 0.1012 # [m]
 		self.wheelBase = 0.207 # [m]
+		self.encCountsPerRev = 3592
 
+		# wheel velocities
 		self.velocityRigthWheel = 0
 		self.velocityLeftWheel = 0
 
+		# the beginning position and orientation
 		self.positionX = 0
 		self.positionY = 0
 		self.angleTheta = 0
+
+		# General Radius of curvature, linear and angular velocity
+		self.radiusOfCurvature = 0
+		self.linearVelocity = 0
+		self.angularVelocity = 0
 
 		self.linearVelocityX = 0
 		self.linearVelocityY = 0
@@ -44,18 +54,39 @@ class Odometry:
 		self.lastTime = rospy.get_time()
 		
 	def callback(self, data):
-		# rospy.loginfo("Encoder ticks are " + str(data.x) + " and " + str(data.y))
+		rospy.loginfo("Encoder ticks are " + str(data.x) + " and " + str(data.y))
 		self.calculateVelocities(data)
+		rospy.loginfo("Radius of curvature" + str(self.radiusOfCurvature))
+		
+		# 
+
+
 
 	def calculateVelocities(self, encoderTicks):
 		self.currentTime = rospy.get_time()
-		self.dt = self.currentTime - self.lastTime		
+		self.dt = self.currentTime - self.lastTime	
 
 		# calculate velocity 
-		self.velocityLeftWheel = encoderTicks.x
+		rightWheelAngularVelocity = (encoderTicks.x * ( 2 * pi / 3592 ) ) / self.dt
+		self.velocityRigthWheel = rightWheelAngularVelocity * self.wheelRadius
+
+		leftWheelAngularVelocity = (encoderTicks.y * ( 2 * pi / 3592 ) ) / self.dt
+		self.velocityLeftWheel = leftWheelAngularVelocity * self.wheelRadius
+
+		rospy.loginfo("velocity left wheel " + str(self.velocityLeftWheel))
+		rospy.loginfo("deltaTime " + str(self.dt))
+
+	# compute the general Radius of curvature
+	# angular and linear velocities
+	def computeGeneralRWV(self):
+		self.radiusOfCurvature = (self.wheelBase / 2) *  \
+			((self.velocityLeftWheel + self.velocityRigthWheel) / \
+			(self.velocityRigthWheel - self.velocityLeftWheel))
+
 
 	# ICC -> Instantaneous Center of Curvature
-	# def computeICC():
+	#def computeICC(self, ):
+
 
 	# def spin(self):
 
