78832668c34ef13438caca02101a84aa65840943
Luis Alfredo Servin Garduño <luis.alfredo.sega@gmail.com>
2015-04-20 21:10:37 -0500

    First sketch of Odometry node finished

--- catkin/src/autonomo_launch/scripts/OdometryNode.py
+++ catkin/src/autonomo_launch/scripts/OdometryNode.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# Always make a division by integer return a float
+# this a feature in python 3 but not in python 2
+from __future__ import division
+
 import rospy
 from geometry_msgs.msg import Vector3
 from nav_msgs.msg import Odometry
@@ -15,7 +19,6 @@ class Odometry:
 		self.rate = rospy.get_param("rate", 5)
 		rospy.loginfo("Rate for this node is " + str(self.rate))
 		rospy.loginfo("started node " + self.node_name)
-		rospy.loginfo("Pi value is " + str(pi))
 
 		# initialize all the variables in class
 
@@ -28,19 +31,19 @@ class Odometry:
 		self.velocityRigthWheel = 0
 		self.velocityLeftWheel = 0
 
-		# the beginning position and orientation
+		# the initial position and orientation
 		self.positionX = 0
 		self.positionY = 0
-		self.angleTheta = 0
+		self.angleTheta = pi / 2 
 
 		# General Radius of curvature, linear and angular velocity
 		self.radiusOfCurvature = 0
-		self.linearVelocity = 0
-		self.angularVelocity = 0
+		self.linearBodyVelocity = 0
+		self.angularBodyVelocity = 0
 
-		self.linearVelocityX = 0
-		self.linearVelocityY = 0
-		self.angularVelocityTheta = 0
+		self.velocityBodyX = 0
+		self.velocityBodyY = 0
+		self.velocityBodyTheta = 0
 
 		self.ICCx = 0
 		self.ICCy = 0
@@ -52,16 +55,38 @@ class Odometry:
 
 		self.currentTime = rospy.get_time()
 		self.lastTime = rospy.get_time()
-		
+
 	def callback(self, data):
-		rospy.loginfo("Encoder ticks are " + str(data.x) + " and " + str(data.y))
 		self.calculateVelocities(data)
-		rospy.loginfo("Radius of curvature" + str(self.radiusOfCurvature))
+		if self.velocityLeftWheel == self.velocityRigthWheel and self.velocityLeftWheel != 0:		
+			rospy.loginfo("Same velocity in both wheel")
+
+			#rospy.loginfo("rWAV = " + str(self.velocityRigthWheel) + " lWAV = " + str(self.velocityLeftWheel))
+			self.linearBodyVelocity = (1/2) * (self.velocityLeftWheel + self.velocityRigthWheel)
+			self.angularBodyVelocity = 0
+
+			# descompose the linear body velocity into the x and y component
+			# in odom frame
+
+			self.velocityBodyX = self.linearBodyVelocity * cos(self.angleTheta)
+			self.velocityBodyY = self.linearBodyVelocity * sin(self.angleTheta)
+
+			#rospy.loginfo("vx = " + str(self.velocityBodyX) + " vy = " + str(self.velocityBodyY))
+
+			# calculate the position
+			self.computeNewPositionSameVelocities()
+		else:
+			rospy.loginfo("Different wheels' velocities")
+			self.computeGeneralRWV()
+			self.computeICC()
+			self.computeVelocities()
+			self.computeNewPositionDifferentVelocites()
+
+		rospy.loginfo("x = " + str(self.positionX) + " y = " + str(self.positionY) \
+			+ " theta = " + str(self.angleTheta * 180 / pi))
+		self.lastTime = self.currentTime
 		
 		# 
-
-
-
 	def calculateVelocities(self, encoderTicks):
 		self.currentTime = rospy.get_time()
 		self.dt = self.currentTime - self.lastTime	
@@ -73,20 +98,48 @@ class Odometry:
 		leftWheelAngularVelocity = (encoderTicks.y * ( 2 * pi / 3592 ) ) / self.dt
 		self.velocityLeftWheel = leftWheelAngularVelocity * self.wheelRadius
 
-		rospy.loginfo("velocity left wheel " + str(self.velocityLeftWheel))
-		rospy.loginfo("deltaTime " + str(self.dt))
-
 	# compute the general Radius of curvature
 	# angular and linear velocities
 	def computeGeneralRWV(self):
-		self.radiusOfCurvature = (self.wheelBase / 2) *  \
+		self.radiusOfCurvature = (self.wheelBase / 2) * \
 			((self.velocityLeftWheel + self.velocityRigthWheel) / \
 			(self.velocityRigthWheel - self.velocityLeftWheel))
 
+		self.angularBodyVelocity = (self.velocityRigthWheel - self.velocityLeftWheel) / \
+			self.wheelBase
 
+		self.linearBodyVelocity = (self.velocityRigthWheel	+ self.velocityLeftWheel) / 2
 	# ICC -> Instantaneous Center of Curvature
-	#def computeICC(self, ):
+	def computeICC(self):
+		self.ICCx = self.positionX - self.radiusOfCurvature * sin(self.angleTheta)
+		self.ICCy = self.positionY + self.radiusOfCurvature * cos(self.angleTheta)
 
+	def computeNewPositionDifferentVelocites(self):
+		self.positionX = cos(self.angularBodyVelocity * self.dt) * (self.positionX - self.ICCx) \
+			- sin(self.angularBodyVelocity * self.dt) * (self.positionY - self.ICCy) \
+			+ self.ICCx
+
+		self.positionY = sin(self.angularBodyVelocity * self.dt) * (self.positionX - self.ICCx) \
+			+ cos(self.angularBodyVelocity * self.dt) * (self.positionY - self.ICCy) \
+			+ self.ICCy
+
+		self.angleTheta = self.angleTheta + self.angularBodyVelocity * self.dt
+
+	def computeNewPositionSameVelocities(self):
+		self.dx = self.velocityBodyX * cos(self.angleTheta) \
+			- self.velocityBodyY * sin(self.angleTheta)
+		self.dx *= self.dt
+
+		self.dy = self.velocityBodyX * sin(self.angleTheta) \
+			+ self.velocityBodyY * cos(self.angleTheta)
+		self.dy *= self.dt
+
+		# robot is straighforward
+		self.dtheta = 0
+
+		self.positionX += self.dx
+		self.positionY += self.dy
+		self.angleTheta += self.dtheta
 
 	# def spin(self):
 
