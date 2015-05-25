a6ca425723c67445ec23da6b3112007bb1b34f70
Luis Servin <luis.alfredo.sega@gmail.com>
2015-05-25 16:18:04 -0500

    upgrade

--- Courses/Statistic Mechanics/Lecture1/direct_pi.py
+++ Courses/Statistic Mechanics/Lecture1/direct_pi.py
@@ -5,7 +5,12 @@ import random
 n_trials = 4000
 n_hits = 0
 for iter in range(n_trials):
+	# here me make a random sampling for x and y
 	x, y = random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)
+	# check if x and y is inside the circle of radius 1 
 	if x**2 + y**2 < 1.0:
+		# if true, aument hits by 1
 		n_hits += 1
+# the relation between the area of circle and square
+# is Pi / 4
 print 4.0 * n_hits / float(n_trials)
--- Courses/Statistic Mechanics/Lecture1/markov_pi.py
+++ Courses/Statistic Mechanics/Lecture1/markov_pi.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
+# MonteCarlo heliport algorithm
 import random
 
+# set the beginning position
 x, y = 1.0, 1.0
+# maximun delta for the trial
 delta = 0.1
 n_trials = 4000
 n_hints = 0
 for i in range(n_trials):
+	# calculate the new delta for the current trial
 	del_x, del_y = random.uniform(-delta, delta), random.uniform(-delta, delta)
+	# check if we are still inside the square
 	if abs(x + del_x) < 1.0 and abs(y + del_y) < 1.0:
+		# if true move to the new position
 		x, y = x + del_x, y + del_y
+	# in the new position check if we are inside the circle
 	if x**2 + y**2 < 1.0:
 		n_hints += 1
 print 4.0 * n_hints / float(n_trials)
