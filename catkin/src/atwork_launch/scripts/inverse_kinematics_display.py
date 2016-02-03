99ce466bec0983cf2b569a23d97e71a1aa408377
Luis Servin <luis.alfredo.sega@gmail.com>
2016-02-02 20:55:10 -0600

    upgrade

--- catkin/src/atwork_launch/scripts/inverse_kinematics_display.py
+++ catkin/src/atwork_launch/scripts/inverse_kinematics_display.py
@@ -6,13 +6,13 @@ import rospy
 import tf
 import geometry_msgs.msg
 
-def broadcast_wheel_transform():
+def broadcast_wheel_transform(msg):
 	br = tf.TransformBroadcaster();
-	br.sendTr0ansform((0, 0, 0),
-					 (0,0,0,0.001*rospy.Time.now()),
+	br.sendTransform((-0.8, 1.0, 0),
+					 tf.transformations.quaternion_from_euler(0, 0.765, 0),
 					 rospy.Time.now(),
-					 "base_footprint",
-					 "base_wheel_back_left")
+					 "base_wheel_back_left",
+					 "base_footprint")
 
 if __name__ == '__main__':
 	rospy.init_node('wheels_rotation_broadcaster')
@@ -23,3 +23,5 @@ if __name__ == '__main__':
 	rospy.Subscriber('/cmd_vel', 
 					 geometry_msgs.msg.Twist,
 					 broadcast_wheel_transform)
+
+	rospy.spin()
