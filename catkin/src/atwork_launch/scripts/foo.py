196f4398b29081ece27f0bbb37bd04d195b075f8
Luis Servin <luis.alfredo.sega@gmail.com>
2016-02-04 19:34:04 -0600

    upgrade

---
+++ catkin/src/atwork_launch/scripts/foo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Header
+
+def talker():
+	rospy.init_node('robot_state_publisher')
+	
+	pub = rospy.Publisher('joint_states', JointState, queue_size=10)
+	rate = rospy.Rate(10)
+
+	state_str = JointState()
+	state_str.header = Header()
+	state_str.header.stamp = rospy.Time.now()
+	
+	state_str.name = ['base_footprint_to_base_wheel', 'base_footprint_to_base_wheel_front_right', 'base_footprint_to_base_wheel_front_left', 'base_footprint_to_base_wheel_back_right']
+	state_str.position = [1, 0.5, -0.5, 2.1]
+	state_str.velocity = []
+	state_str.effort = []
+
+	while not rospy.is_shutdown():
+		state_str.header.stamp = rospy.Time.now()
+		pub.publish(state_str)
+		rate.sleep()
+
+
+if __name__ == '__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
