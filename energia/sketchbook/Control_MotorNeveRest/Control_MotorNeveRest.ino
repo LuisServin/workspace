de5ec92fbeb6396dd0797bec4d5973903d3333ad
Luis Servin <luis.alfredo.sega@gmail.com>
2016-03-31 16:17:48 -0600

    upgrade

--- energia/sketchbook/Control_MotorNeveRest/Control_MotorNeveRest.ino
+++ energia/sketchbook/Control_MotorNeveRest/Control_MotorNeveRest.ino
@@ -16,10 +16,13 @@ volatile long pos1 = 0;
 
 // Create the first motor object.
 // here the pinmode in made inside instance creation
-motor Mot1(M1_DIR, M1_PWM, M1_ENC1, M1_ENC2);
+Motor Mot1(M1_DIR, M1_PWM, M1_ENC1, M1_ENC2);
 
 // local variables
-int angPosition = 0;
+long steps = 0;
+float as_steps;
+
+/**************** SETUP *******************/
 
 void setup()
 {
@@ -31,18 +34,21 @@ void setup()
   attachInterrupt(M1_ENC2, iM1E2, CHANGE);
 
   // set pwm and direction for motor
-  Mot1.setPwm(50);
-  Mot1.setDirection(LOW);
+  Mot1.setPwm(100);
+  Mot1.setDirection(HIGH);
+  // write electronic values
+  Mot1.runMotor();
 }
+
+/**************** LOOP *******************/
 
 void loop()
 {
-  // write electronic values
-  Mot1.runMotor();
-  delay(500);
+  delay(50);
 
-  angPosition = Mot1.stepsIncrement();
-  Serial.println(angPosition);
+  Mot1.calculateKinematicVariables();
+  as_steps = Mot1.getAngularSpeedRad();
+  Serial.println(as_steps);
 }
 
 // assign interruption function for every encoder respectively
